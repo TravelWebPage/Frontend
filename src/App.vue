a7deef4539883fac00bc9dce0248d476a35afc8e
rudgns517 <rudgns517@naver.com>
2021-05-01 00:05:59 +0900

    app-bar, slide show update

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,112 @@
 <template>
-  <v-card
-    class="mx-auto overflow-hidden"
-    height="400"
-  >
-    <v-app-bar
-      color="deep-purple"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <v-app>
+        <v-app-bar app flat>
+            <v-btn icon>
+                <v-icon 
+                    large 
+                    right 
+                    dark 
+                    color="yellow darken-2"
+                >fa-map-marker-alt
+                </v-icon>
+            </v-btn>
+            <v-app-bar-title>여기갈래</v-app-bar-title>
 
-      <v-toolbar-title>Title</v-toolbar-title> 
-    </v-app-bar>
+                <v-spacer />
+     
+                      <v-responsive max-width="260">
+                        <v-text-field
+                          placeholder="검색 시작하기"
+                          
+                          dense
+                          flat
+                          hide-details
+                          rounded
+                          solo-inverted
+                          >
+                        </v-text-field>
+                        
+                    </v-responsive>
+                    <v-btn icon>
+                            <v-icon color="yellow darken-2">
+                                fa-search
+                            </v-icon>
+                        </v-btn>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+                <v-spacer />
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+            <v-btn text>공지사항</v-btn>
+            <v-btn text>마이페이지</v-btn>
+            
+        </v-app-bar>
+            
+        <v-content>
+            <v-carousel
+                cycle
+                height="500"
+                hide-delimiter-background
+                show-arrows-on-hover
+  >             
+                <template v-slot:prev="{ on, attrs }">
+                  <v-btn
+                    color="success"
+                    v-bind="attrs"
+                    v-on="on"
+                  >Previous slide</v-btn>
+                </template>
+                <template v-slot:next="{ on, attrs }">
+                  <v-btn
+                    color="info"
+                    v-bind="attrs"
+                    v-on="on"
+                  >Next slide</v-btn>
+                </template>
+                <v-carousel-item
+                  v-for="(slide, i) in slides"
+                  :key="i"
+                >
+                  <v-sheet
+                    :color="colors[i]"
+                    height="150%"
+                  >
+                    <v-row
+                      class="fill-height"
+                      align="center"
+                      justify="center"
+                    >
+                      <div class="display-3">
+                        {{ slide }} Slide
+                      </div>
+                    </v-row>
+                  </v-sheet>
+                </v-carousel-item>
+             </v-carousel>
+        </v-content>
+
+        
+
+    </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
+    data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    },
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify'
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false
 
@@ -8,3 +10,7 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vuetify, {
+  iconfont: 'fa'
+ })
