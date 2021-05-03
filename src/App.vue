1dbf1e69aecd53c95c3769bfd37963084b6d27ef
rudgns517 <rudgns517@naver.com>
2021-05-03 20:42:25 +0900

    input images in carousel

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
     <v-app>
-        <v-app-bar app flat>
-            <v-btn icon>
-                <v-icon 
-                    large 
-                    right 
-                    dark 
-                    color="yellow darken-2"
-                >fa-map-marker-alt
-                </v-icon>
-            </v-btn>
-            <v-app-bar-title>여기갈래</v-app-bar-title>
+        <v-app-bar color="white" flat>
+                
+                <img class="ml-3" @click="go_home" :src="require('./assets/logo.png')" height="45" />
 
                 <v-spacer />
      
                       <v-responsive max-width="260">
                         <v-text-field
                           placeholder="검색 시작하기"
-                          
-                          dense
                           flat
                           hide-details
                           rounded
@@ -35,78 +25,39 @@
 
                 <v-spacer />
 
+            <v-btn text>여행지 추천</v-btn>
             <v-btn text>공지사항</v-btn>
-            <v-btn text>마이페이지</v-btn>
             
         </v-app-bar>
             
-        <v-content>
-            <v-carousel
-                cycle
-                height="500"
-                hide-delimiter-background
-                show-arrows-on-hover
-  >             
-                <template v-slot:prev="{ on, attrs }">
-                  <v-btn
-                    color="success"
-                    v-bind="attrs"
-                    v-on="on"
-                  >Previous slide</v-btn>
-                </template>
-                <template v-slot:next="{ on, attrs }">
-                  <v-btn
-                    color="info"
-                    v-bind="attrs"
-                    v-on="on"
-                  >Next slide</v-btn>
-                </template>
-                <v-carousel-item
-                  v-for="(slide, i) in slides"
-                  :key="i"
-                >
-                  <v-sheet
-                    :color="colors[i]"
-                    height="150%"
-                  >
-                    <v-row
-                      class="fill-height"
-                      align="center"
-                      justify="center"
-                    >
-                      <div class="display-3">
-                        {{ slide }} Slide
-                      </div>
-                    </v-row>
-                  </v-sheet>
-                </v-carousel-item>
-             </v-carousel>
-        </v-content>
-
+    <v-main>
+      <Home />
+    </v-main>
         
 
     </v-app>
 </template>
 
 <script>
+
+import Home from "./components/Home";
+
   export default {
+    name: 'App',
+
+    components: {
+      Home,
+    },
+
     data () {
       return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
+        go_home(){
+          console.log('go home')
+        }
       }
+    },
+    methods:{
+      
     },
   }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-carousel
+    cycle
+    height="auto"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+      :src="slide"
+      style="width: 70%; margin: auto;"
+    >
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        slides: [
+          require('@/assets/images/강원도.png'),
+          require('@/assets/images/경기도.png'),
+          require('@/assets/images/경상도.png'),
+          require('@/assets/images/전라도.png'),
+          require('@/assets/images/충청도.png'),
+        ],
+      }
+    },
+  }
+</script>
